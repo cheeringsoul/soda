d605c59e3802d739fd8e3d6593b80d077d3b379c
york <york@pineal.com>
2025-01-02 17:03:18 +0800

    update

--- src/main/java/com/soda/Engine.java
+++ src/main/java/com/soda/Engine.java
@@ -2,13 +2,11 @@ package com.soda;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.*;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 
 interface Callback {
@@ -18,9 +16,8 @@ interface Callback {
 
 @Slf4j
 public class Engine {
-    private static final int QUEUE_CAPACITY = 10;
     private final Map<Class<?>, List<Callback>> eventHandlers = new HashMap<>();
-    private final BlockingQueue<Event> queue = new ArrayBlockingQueue<>(QUEUE_CAPACITY);
+    private final Queue<Event> queue = new ConcurrentLinkedQueue<>();
     ExecutorService executorService;
     public static final Engine INSTANCE = new Engine();
 
@@ -30,43 +27,40 @@ public class Engine {
     }
 
     public <T extends Application> void addApplication(Class<T> clazz) {
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        T instance = null;
         try {
-            Method[] declaredMethods = clazz.getDeclaredMethods();
-            T instance = clazz.getDeclaredConstructor().newInstance();
-            instance.setEngine(this);
-
-            for (Method method : declaredMethods) {
-                method.setAccessible(true);
-                if (method.isAnnotationPresent(EventHandler.class)) {
-                    System.out.println("Method with EventHandler annotation: " + method.getName());
-                    Parameter[] parameters = method.getParameters();
-                    if (parameters.length != 1) {
-                        throw new RuntimeException("Method with EventHandler annotation must have exactly one parameter");
-                    }
-                    if (!Event.class.isAssignableFrom(parameters[0].getType())) {
-                        throw new RuntimeException("Method with EventHandler annotation must have exactly one parameter of type Event");
-                    }
-                    eventHandlers.computeIfAbsent(parameters[0].getType(), k -> new ArrayList<>()).add((Event event) -> {
-                        try {
-                            method.invoke(instance, event);
-                        } catch (Exception e) {
-                            log.error("Error invoking method", e);
-                        }
-                    });
-
-                }
-            }
-        } catch (Exception e) {
-            log.error("Error adding application", e);
+            instance = clazz.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
         }
+        instance.setEngine(this);
+        for (Method method : declaredMethods) {
+            method.setAccessible(true);
+            if (method.isAnnotationPresent(EventHandler.class)) {
+                log.info("Method with EventHandler annotation: {}", method.getName());
+                Parameter[] parameters = method.getParameters();
+                if (parameters.length != 1) {
+                    throw new RuntimeException("Method with EventHandler annotation must have exactly one parameter");
+                }
+                if (!Event.class.isAssignableFrom(parameters[0].getType())) {
+                    throw new RuntimeException("Method with EventHandler annotation must have exactly one parameter of type Event");
+                }
+                final T finalInstance = instance;
+                eventHandlers.computeIfAbsent(parameters[0].getType(), k -> new ArrayList<>()).add((Event event) -> {
+                    try {
+                        method.invoke(finalInstance, event);
+                    } catch (Exception e) {
+                        log.error("Error invoking method", e);
+                    }
+                });
+            }
+        }
+
     }
 
     public void pubEvent(Event event) {
-        try {
-            queue.put(event);
-        } catch (InterruptedException e) {
-            log.error("Error publishing event", e);
-        }
+        queue.add(event);
     }
 
     public void run() {
@@ -74,22 +68,20 @@ public class Engine {
         pubEvent(new ExampleEvent1("2", "Example Event 1"));
 
         while (true) {
-            try {
-                Event event = queue.take();
-                List<Callback> callbacks = eventHandlers.get(event.getClass());
-                if (callbacks == null) {
-                    continue;
+            Event event = queue.poll();
+            if (event == null) {
+                continue;
+            }
+            List<Callback> callbacks = eventHandlers.get(event.getClass());
+            if (callbacks == null) {
+                continue;
+            }
+            for (Callback callback : callbacks) {
+                try {
+                    callback.call(event);
+                } catch (Exception e) {
+                    log.error("Error calling callback", e);
                 }
-                for (Callback callback : callbacks) {
-                    try {
-                        callback.call(event);
-                    } catch (Exception e) {
-                        log.error("Error calling callback", e);
-                    }
-                }
-            } catch (InterruptedException e) {
-                log.error("Error running engine", e);
-                break;
             }
         }
 
